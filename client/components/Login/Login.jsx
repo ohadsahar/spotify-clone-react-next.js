88932bb6655071f3d6bba7edae24c23581be0fdf
Ohad Sahar <ohad@any-app.com>
2021-07-21 09:31:11 +0300

    styled a little bit the login page

--- client/components/Login/Login.jsx
+++ client/components/Login/Login.jsx
@@ -1,4 +1,5 @@
 import { SPOTIFY_CLIENT_ID, SPOTIFY_URL, URI, SPOTIFY_PERMISSION } from "@/config/index";
+import { LoginWrapper } from "./StyledLogin";
 
 const url = `${SPOTIFY_URL}?client_id=${SPOTIFY_CLIENT_ID}&response_type=code
 &redirect_uri=${URI}&scope=${SPOTIFY_PERMISSION}`
@@ -6,7 +7,10 @@ const url = `${SPOTIFY_URL}?client_id=${SPOTIFY_CLIENT_ID}&response_type=code
 const Login = () => {
 
     return (
-        <a className="btn btn-success btn-lg" href={url}>Login with spotify</a>
+        <LoginWrapper>
+            <a className="btn btn-success btn-lg" href={url}>Login with spotify</a>
+        </LoginWrapper>
+
     )
 }
 
--- client/components/Login/StyledLogin.js
+++ client/components/Login/StyledLogin.js
@@ -1,6 +1,14 @@
 import styled from 'styled-components';
 
 export const LoginWrapper = styled.div`
-
+height:100vh;
+background-color: black;
+display: flex;
+justify-content: center;
+align-items: center;
+ a {
+     color: white;
+     font-size: 2rem;
+ }
 
 `;
